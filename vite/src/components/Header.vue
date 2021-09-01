d1b44ab14a960d5b88b7c3c2d9c75a07f1567325
kght6123 <i.feeling.song@gmail.com>
2021-09-02 00:31:09 +0900

    レスポンシブ対応

--- vite/src/components/Header.vue
+++ vite/src/components/Header.vue
@@ -1,75 +1,65 @@
 <template>
   <div class="fixed-header glass-dark">
-    <div class="flex-none hidden lg:flex">
-      <button class="btn btn-square btn-ghost">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          class="inline-block w-6 h-6 stroke-current"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          ></path>
-        </svg>
-      </button>
-    </div>
-    <div class="flex-1 hidden px-2 mx-2 lg:flex">
+    <div class="navbar-start">
+      <!-- TODO:後でAmazonの手を振ってくれるアニメーションみたいなの入れたい
       <span v-if="state.isLoggedin" class="text-lg font-bold"
         >こんにちは、&nbsp;{{ state.displayName }}&nbsp;さん</span
       >
+      -->
     </div>
-    <div v-if="state.isLoggedin" class="flex-1 lg:flex-none">
+    <div v-if="state.isLoggedin" class="flex-1 navbar-center">
       <div class="form-control">
-        <input
-          v-model="data.keyword"
-          type="text"
-          placeholder="Search"
-          class="input input-ghost"
-        />
+        <div class="flex space-x-2">
+          <input
+            v-model="data.keyword"
+            type="text"
+            placeholder="Search"
+            class="input input-ghost"
+          />
+          <button
+            class="btn btn-square btn-ghost"
+            @click="searchIndexesKeyword"
+          >
+            <component
+              :is="search"
+              class="inline-block w-6 h-6 mr-1 fill-current"
+            />
+          </button>
+        </div>
       </div>
     </div>
-    <div v-if="state.isLoggedin" class="flex-none">
-      <button class="btn btn-square btn-ghost" @click="searchIndexesKeyword">
-        <component
-          :is="search"
-          class="inline-block w-6 h-6 mr-1 fill-current"
-        />
-      </button>
-    </div>
-    <div v-if="state.isLoggedin" class="flex-none">
-      <button class="btn btn-square btn-ghost">
-        <component :is="heart" class="inline-block w-6 h-6 mr-1 fill-current" />
-      </button>
-    </div>
-    <div class="flex-none">
-      <router-link class="btn btn-square btn-ghost" to="/" tag="button">
-        <component :is="home" class="inline-block w-6 h-6 mr-1 fill-current" />
-      </router-link>
-    </div>
-    <div v-if="state.isLoggedin" class="flex-none">
-      <router-link class="btn btn-square btn-ghost" to="/setting" tag="button">
-        <component :is="cog" class="inline-block w-6 h-6 mr-1 fill-current" />
-      </router-link>
-    </div>
-    <div v-if="state.isLoggedin" class="flex-none">
-      <atoms-button class="btn btn-square btn-ghost" @click="signout()">
+    <div class="navbar-end">
+      <atoms-button
+        v-if="state.isLoggedin"
+        class="btn btn-square btn-ghost"
+        @click="signout()"
+      >
         <component
           :is="signOut"
           class="inline-block w-6 h-6 mr-1 fill-current"
         />
       </atoms-button>
-    </div>
-    <div v-if="state.isLoggedin" class="flex-none">
-      <div class="avatar">
+      <div v-if="state.isLoggedin" class="avatar">
         <div class="w-10 h-10 m-1 mask mask-squircle">
           <img :src="state.photoURL" />
         </div>
       </div>
     </div>
+    <!-- div v-if="state.isLoggedin" class="flex-none">
+      <button class="btn btn-square btn-ghost">
+        <component :is="heart" class="inline-block w-6 h-6 mr-1 fill-current" />
+      </button>
+    </div-->
+    <!-- div class="flex-none">
+      <router-link class="btn btn-square btn-ghost" to="/" tag="button">
+        <component :is="home" class="inline-block w-6 h-6 mr-1 fill-current" />
+      </router-link>
+    </div-->
+    <!-- div v-if="state.isLoggedin" class="flex-none">
+      <router-link class="btn btn-square btn-ghost" to="/setting" tag="button">
+        <component :is="cog" class="inline-block w-6 h-6 mr-1 fill-current" />
+      </router-link>
+    </div-->
   </div>
 </template>
 
--- vite/src/views/MyPage.vue
+++ vite/src/views/MyPage.vue
@@ -19,14 +19,51 @@
           </div>
         </div>
       </div>
+      <div class="mt-72 hero">
+        <div class="btn-group">
+          <button class="btn btn-active">お気に入り</button>
+          <button class="btn">たぐ1</button>
+          <button class="btn">たぐ2</button>
+          <button class="btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              class="inline-block w-4 stroke-current"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
-    <div class="-mt-96 hero min-h-[24rem] pb-48">
+    <div
+      class="
+        -mt-36
+        sm:-mt-64
+        md:-mt-28
+        lg:-mt-56
+        xl:-mt-52
+        2xl:-mt-80
+        hero
+        min-h-[24rem]
+        pb-48
+      "
+    >
       <div
         class="
           w-11/12
           max-w-[100vw]
+          sm:masonry-2-col
           md:masonry-2-col
           lg:masonry-3-col
+          xl:masonry-4-col
+          2xl:masonry-5-col
           before:box-inherit
           after:box-inherit
           space-y-4
@@ -45,7 +82,9 @@
               {{ item.title }}
               <div class="mx-2 badge badge-secondary">NEW</div>
             </h2>
-            <p>{{ item.url }}</p>
+            <a target="_blank" :href="item.url" class="link link-secondary">{{
+              item.url
+            }}</a>
             <div class="justify-end card-actions">
               <button class="btn btn-secondary">More info</button>
             </div>
